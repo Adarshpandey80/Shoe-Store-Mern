3420cf9632a23aae0857c4bfbc9bbdfbd3ad93e8
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-01-12 22:09:22 +0530

    `Removed comment function`

--- Server/controllers/paymentController.js
+++ Server/controllers/paymentController.js
@@ -33,50 +33,6 @@ const orderPayment = async (req, res) => {
 }
 
 
-// const verifyPayment = async (req, res) => {
-//     try {
-//         const {
-//             razorpay_orderID,
-//             razorpay_paymentID,
-//             razorpay_signature,
-//             userId,
-//             amount,
-//             products } = req.body;
-//             console.log(req.body)
-//         const sign = razorpay_orderID + "|" + razorpay_paymentID;
-//         const resultSign = crypto
-//             .createHmac("sha256", process.env.KEY_SECRET)
-//             .update(sign.toString())
-//             .digest("hex");
-
-       
-
-//         // ❌ If signature invalid
-//         if (razorpay_signature !== expectedSign) {
-//             return res.status(400).json({ message: "Payment verification failed" });
-//         }
-
-//         // ✅ SAVE ORDER
-//         const order = await OrderModel.create({
-//             userId,
-//             orderId: razorpay_orderID,
-//             paymentId: razorpay_paymentID,
-//             amount,
-//             products,
-//             paymentStatus: "success"
-//         });
-
-//         res.status(200).json({
-//       message: "Payment verified successfully",
-//       order,
-//     });
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ message: "Internal Server Error!" });
-//     }
-// }
-
 
 const verifyPayment = async (req, res) => {
   try {
